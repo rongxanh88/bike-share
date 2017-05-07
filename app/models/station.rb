821f711019176ee74339bb408d1db5b56088bd0d
Benjamin Ross <benjaminross1001@gmail.com>
2017-05-07 13:49:44 -0600

    Creates method for newest and oldest stations

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Station < ActiveRecord::Base
   belongs_to :city
   validates :name,
@@ -6,4 +8,29 @@ class Station < ActiveRecord::Base
             :dock_count,
             :installation_date,
             presence: true
+
+
+  def self.generate_list_of_names(object)
+    if object.length == 1
+      Array.new << object.first.name
+    else
+      object.map{ | item | item.name }.flatten
+    end
+  end
+
+  def self.oldest_date(attribute)
+    self.minimum(attribute)
+  end
+
+  def self.newest_date(attribute)
+    self.maximum(attribute)
+  end
+
+  def self.newest_stations
+    generate_list_of_names(self.where(installation_date: self.newest_date(:installation_date)))
+  end
+
+  def self.oldest_stations
+    generate_list_of_names(self.where(installation_date: self.oldest_date(:installation_date)))
+  end
 end
