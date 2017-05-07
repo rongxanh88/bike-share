cabe8b8d485d13870b7be19572cac7a35c14d9c6
rongxanh88 <rongxanh88@gmail.com>
2017-05-06 20:07:14 -0600

    made changes to index.erb file

--- app/controllers/bike_share_app.rb
+++ app/controllers/bike_share_app.rb
@@ -7,4 +7,16 @@ class BikeShareApp < Sinatra::Base
     erb :"stations/index"
     # haml :"stations/index"
   end
+
+  get '/stations/:id/edit' do
+    @station = Station.find_by(id: params[:id])
+
+    erb :"stations/#{@station.id}/edit"
+  end
+
+  delete '/station/:id' do
+    Station.find_by(id: params[:id]).destroy
+
+    redirect '/stations'
+  end
 end
