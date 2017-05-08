82f06cdbf6dee7268196c4626a707be4c7a8b6e4
Carl Richmond <richmond.carl@outlook.com>
2017-05-08 10:02:42 -0600

    renamed method to be more specific in controller

--- app/controllers/bike_share_app.rb
+++ app/controllers/bike_share_app.rb
@@ -53,13 +53,13 @@ class BikeShareApp < Sinatra::Base
                           dock_count: params[:dock_count],
                           installation_date: params[:installation_date])
 
-    city = create_city_relationship(params[:city])
+    city = find_city_id(params[:city])
     station.update_attributes(:city_id => city)
     station.save
     redirect '/stations'
   end
 
-  def create_city_relationship(params)
+  def find_city_id(params)
     city = City.find_by(name: params)
     city.id
   end
