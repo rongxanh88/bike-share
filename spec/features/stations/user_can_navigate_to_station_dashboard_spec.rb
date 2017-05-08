eabcdf0eb033a532054ac8598c223f71b2623522
Carl Richmond <richmond.carl@outlook.com>
2017-05-08 16:14:49 -0600

    work on feature testing station dashboard

---
+++ spec/features/stations/user_can_navigate_to_station_dashboard_spec.rb
@@ -0,0 +1,12 @@
+require_relative '../../spec_helper'
+require_relative '../../station_city_creation_module'
+require "pry"
+
+RSpec.describe "user can navigate to different station dashboards" do
+include StationCityCreator
+
+  it "they see station dashboards" do
+    
+  end
+
+end
