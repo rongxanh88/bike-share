c3c7cc8681b4db29006df9f87f76588210a53b12
Carl Richmond <richmond.carl@outlook.com>
2017-05-09 13:50:23 -0600

    additions to weather spec

--- spec/models/weather_spec.rb
+++ spec/models/weather_spec.rb
@@ -54,9 +54,45 @@ RSpec.describe Trip do
 
   describe "model method" do
     it "returns max temp" do
-      max_temp = Weather.maximum(:max_temp)
-      max_temp_city = Weather.maximum(:max_temp)
-      binding.pry
+      weather_by_date = Weather.where(date: "2013-12-22")
+      max_temp = weather_by_date.maximum(:max_temp)
+      expect(max_temp).to eq(81.0)
+    end
+
+    it "returns mean temp" do
+      weather_by_date = Weather.where(date: "2013-12-22")
+      mean_temp = weather_by_date.average(:mean_temp).to_f
+      expect(mean_temp).to eq(72.0)
+    end
+
+    it "returns min temp" do
+      weather_by_date = Weather.where(date: "2013-12-22")
+      min_temp = weather_by_date.average(:min_temp).to_f
+      expect(min_temp).to eq(63.5)
+    end
+
+    it "returns Mean Humidity" do
+      weather_by_date = Weather.where(date: "2013-12-22")
+      mean_humidity = weather_by_date.average(:mean_humidity).to_f
+      expect(mean_humidity).to eq(70.0)
+    end
+
+    it "Mean Visibility (in Miles)" do
+      weather_by_date = Weather.where(date: "2013-11-23")
+      mean_visibility = weather_by_date.average(:mean_visibility).to_f
+      expect(mean_visibility).to eq(11.0)
+    end
+
+    it "returns mean wind speed" do
+      weather_by_date = Weather.where(date: "2013-12-22")
+      mean_wind_speed = weather_by_date.average(:mean_wind_speed).to_f
+      expect(mean_wind_speed).to eq(8.0)
+    end
+
+    it "returns precipitation" do
+      weather_by_date = Weather.where(date: "2013-12-22")
+      precipitation = weather_by_date.average(:precipitation).to_f
+      expect(precipitation).to eq(0.0)
     end
   end
 end
