44fbb3c51646b295800afe32b70ae72a6d01b339
Bao Nguyen <rongxanh88@gmail.com>
2017-05-11 09:13:27 -0600

    Update zip_code.rb

--- app/models/zip_code.rb
+++ app/models/zip_code.rb
@@ -1,3 +1,7 @@
 class ZipCode < ActiveRecord::Base
   validates :zip_code, presence: true, uniqueness: true
+  
+  def self.validate(zip_code)
+    zip_code.to_s.split("").size > 5 ? 0 : zip_code
+  end
 end
