aa19ac4b98ceb56976ef55b74f1295698e5e484b
Bao Nguyen <rongxanh88@gmail.com>
2017-05-11 09:12:51 -0600

    Update zip_code_spec.rb

--- spec/models/zip_code_spec.rb
+++ spec/models/zip_code_spec.rb
@@ -15,5 +15,11 @@ RSpec.describe ZipCode do
 
       expect(zip).to_not be_valid
     end
+    
+    it "returns zero if zip code is too long" do
+      zip_code = ZipCode.validate(123456)
+      
+      expect(zip_code).to eq(0)
+    end
   end
 end
